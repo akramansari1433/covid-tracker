537611497090a6156883ab8dfa288be7991aebbf
akramansari1433 <70878208+akramansari1433@users.noreply.github.com>
2021-09-17 21:19:55 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,76 +1,77 @@
-import Cards from './components/Cards/Cards';
-import StatePicker from './components/StatePicker/StatePicker';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import { useEffect,useState } from 'react';
-import Particles from 'react-particles-js';
-import './App.css';
-
+import React from "react";
+import Cards from "./components/Cards/Cards";
+import StatePicker from "./components/StatePicker/StatePicker";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { useEffect, useState } from "react";
+import Particles from "react-particles-js";
+import axios from "axios";
+import "./App.css";
 
 const particlesOptions = {
-  particles: {
-    number: {
-      value: 100,
-      density: {
-        enable: true,
-        value_area: 2000
-      }
-    }
-  }
-}
+   particles: {
+      number: {
+         value: 100,
+         density: {
+            enable: true,
+            value_area: 2000,
+         },
+      },
+   },
+};
 
 function App() {
+   const [data, setData] = useState([]);
+   const [fetchedState, setFetchedState] = useState([]);
+   const [selectedState, setSelectedState] = useState("Total");
 
-  const [data,setData]=useState([]);
-  const [fetchedState,setFetchedState]=useState([]);
-  const [selectedState,setSelectedState]=useState("Total")
+   const getCovidData = async (state) => {
+      try {
+         const res = await axios.get("https://api.covid19india.org/data.json");
+         const { statewise } = await res.json();
+         const data = statewise.filter((data) => {
+            if (data.state === state) {
+               return setData(data);
+            }
+            return data.state === "Total";
+         });
+         console.log(data);
+      } catch (err) {
+         console.log(err);
+      }
+   };
 
-  const getCovidData=async(state)=>{
-    try{
-        const res=await fetch("https://api.covid19india.org/data.json")
-        const {statewise}=await res.json();
-        const data=statewise.filter(data=>{
-          if(data.state===state){
-            return setData(data)
-          }
-          return data.state==="Total"
-        })
-        console.log(data)
-    }
-    catch(err){
-        console.log(err);
-    }
-  }
+   const statePicker = async () => {
+      try {
+         const res = await axios.get("https://api.covid19india.org/data.json");
+         const { statewise } = await res.json();
+         setFetchedState(statewise.map((data) => data.state));
+      } catch (error) {
+         console.log(error);
+      }
+   };
 
-  const statePicker=async()=>{
-    try {
-      const res=await fetch("https://api.covid19india.org/data.json")
-      const {statewise}=await res.json();
-      setFetchedState(statewise.map((data)=>data.state))
-      
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(()=>{
+   useEffect(() => {
       getCovidData(selectedState);
       statePicker();
-  },[selectedState])
+   }, [selectedState]);
 
-  const handleStateChange=async(state)=>{
+   const handleStateChange = async (state) => {
       setSelectedState(state);
-  }
+   };
 
-  return (
-    <>
-        <Particles className='particles' params={particlesOptions}/>
-        <Header/>
-        <StatePicker states={fetchedState} handleStateChange={handleStateChange} />
-        <Cards data={data}/>
-        <Footer/>
-    </>
-  );
+   return (
+      <div>
+         <Particles className="particles" params={particlesOptions} />
+         <Header />
+         <StatePicker
+            states={fetchedState}
+            handleStateChange={handleStateChange}
+         />
+         <Cards data={data} />
+         <Footer />
+      </div>
+   );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-    return (
-    
-        <div className="tc">
-            <h3 className="white">🔴 Live</h3>
-            <h2 className="b white">COVID-19 TRACKER</h2>
-        </div>
-    )
+   return (
+      <div className="tc">
+         <h3 className="white">🔴 Live</h3>
+         <h2 className="b white">COVID-19 TRACKER</h2>
+      </div>
+   );
 }
 
-export default Header
+export default Header;
